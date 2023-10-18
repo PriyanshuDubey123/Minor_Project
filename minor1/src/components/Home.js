fc7ab928f69eab86eebc86ce69c5bdca3d3c69b7
VaishnaviChouksey <vaishnavichouksey9@gmail.com>
2023-10-18 08:26:41 +0530

    update home page

--- minor1/src/components/Home.js
+++ minor1/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 <div className='text-5xl font-bold pb-2'>Discover a new world of learning. </div>
 <div className='text-4xl font-bold pb-2'>Classroom enviroment at home. </div>
 <div className='text-3xl font-bold pb-2'>Learn from top educators. </div>
-<div className='pt-2 pl-[300px]'>
+<div className='pt-2 pl-[300px] '>
 <div className='bg-indigo-700 text-white px-3 py-2 rounded-[10px] hover:bg-blue-600 hover:text-slate-950 w-[150px] '>Visit Courses</div>
 </div>
 </div>
