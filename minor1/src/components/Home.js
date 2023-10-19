cef733d9fc70a5288e04dad81f673a1ea4dc11dd
VaishnaviChouksey <vaishnavichouksey9@gmail.com>
2023-10-20 02:15:22 +0530

    update home page

--- minor1/src/components/Home.js
+++ minor1/src/components/Home.js
@@ -3,15 +3,15 @@ const Home = () => {
   return (
     <>
     <div className='flex h-[660px] w-full justify-center text-center pt-3'> 
-    <div className='flex-col w-[800px] pt-[250px] text-center text-sky-950'>
+    <div className='flex-col w-[780px] pt-[230px] text-center text-sky-950'>
 <div className='text-5xl font-bold pb-2'>Discover a new world of learning. </div>
 <div className='text-4xl font-bold pb-2'>Classroom enviroment at home. </div>
 <div className='text-3xl font-bold pb-2'>Learn from top educators. </div>
-<div className='pt-2 pl-[300px] '>
-<div className='bg-indigo-700 text-white px-3 py-2 rounded-[10px] hover:bg-blue-600 hover:text-slate-950 w-[150px] '>Visit Courses</div>
+<div className='pt-2 '>
+<button className='bg-indigo-700 text-white px-3 py-2 rounded-[10px] hover:bg-blue-600 hover:text-slate-950  '>Visit Courses</button>
 </div>
 </div>
-    <img className='w-[600px]' src="https://png.pngtree.com/png-vector/20200517/ourmid/pngtree-flat-illustration-of-online-course-png-image_2207493.jpg" alt="" />
+   <img className='w-[630px] pt-0' src="https://o.remove.bg/downloads/2790741c-81a5-4f4e-b998-f72f4957d404/img-1-removebg-preview.png" alt="" />
     </div>
     </>
   )
--- minor1/src/components/Navbar.js
+++ minor1/src/components/Navbar.js
@@ -4,15 +4,17 @@ const navbar = () => {
   return (
     <>
     <nav className='w-full md:h-14 bg-slate-50 md:flex justify-between items-center px-4 md:px-5'>
-      <div className="text-2xl font-bold  text-indigo-700 w-10">
+      <NavLink className="text-2xl font-bold  text-indigo-700 w-10" to="/">
       Ulearn
-      </div>
+      </NavLink>
       <ul className='flex font-semibold text-[18px] text-indigo-700 w-[1200px] pt-1'>
         <NavLink className='mx-[10px] cursor-pointer' to="/courses">Courses</NavLink>
         <NavLink className='mx-[10px] cursor-pointer' to="/about">About Us</NavLink> 
         <NavLink className='mx-[10px] cursor-pointer' to="/contact">Contact Us</NavLink> 
       </ul>
-      <div className='bg-indigo-700 text-white px-3 py-2 rounded-[10px] hover:bg-blue-600 hover:text-slate-950'>Login/SignUp</div>
+      <div className='pt-2'>
+      <button className='bg-indigo-700 text-white px-3 py-2 rounded-[10px] hover:bg-blue-600 hover:text-slate-950'>Login/SignUp</button>
+      </div>
     </nav>
     </>
   )
